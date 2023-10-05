ff2d7b85e7e6a346884f4053eb2751b9450b5ab8
Aryan Gupta <aryangupta07075@gmail.com>
2023-10-05 13:19:50 +0530

    Added html pages

--- userAuth/urls.py
+++ userAuth/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("student/signup", views.student_signup, name="student_signup"),
+    path("college/signup", views.college_signup, name="college_signup"),
+    path("college/login", views.college_login, name="college_login"),
+    path("student/login", views.student_login, name="student_login"),
 ]
--- userAuth/views.py
+++ userAuth/views.py
@@ -1,2 +1,51 @@
 from django.shortcuts import render
+from .models import Student, College
 
+
+def student_signup(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        username = request.POST["username"]
+        password = request.POST["password"]
+        conf_password = request.POST["conf-password"]
+        college = request.POST["college"]
+        university = request.POST["university"]
+        course = request.POST["course"]
+        year = request.POST["year"]
+        mobile_no = request.POST["mobile-number"]
+
+
+def student_login(request):
+    pass
+
+
+def college_signup(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        username = request.POST["username"]
+        password = request.POST["password"]
+        conf_password = request.POST["conf-password"]
+        college = request.POST["college"]
+        university = request.POST["university"]
+        course = request.POST["course"]
+        year = request.POST["year"]
+        email = request.POST["email"]
+        mobile_no = request.POST["mobile-number"]
+        student = Student(
+            name=name,
+            username=username,
+            password=password,
+            college=college,
+            university=university,
+            course=course,
+            year=year,
+            email=email,
+            mobile_no=mobile_no
+        )
+        student.save()
+
+    return render(request, "userAuth/student_sign_up.html")
+
+
+def college_login(request):
+    pass
